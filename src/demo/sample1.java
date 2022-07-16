1b0265a97842430f4ac58d57b530086d67df6aa8
SAMSUNG <SAMSUNG@SAMSUNG-PC>
2022-07-16 18:23:03 +0530

    [Sheetal]

---
+++ src/demo/sample1.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class sample1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+		System.out.println("HI");
+		System.out.println("I am Sheetal");
+		
+	}
+
+}
